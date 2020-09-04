5a8096741f0edc4f6d16a2d99ec272cfa9ab4dae
Tyler Pacheco <tyler.232528@nv.ccsd.net>
2020-09-04 21:23:02 +0000

    Finished StudentDesk, TimeStamp, SecondsPastMidnight

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,18 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int r1 = scanner.nextInt();
+        int r2 = scanner.nextInt();
+        int r3 = scanner.nextInt();
 
         /*
          *  your code goes here
          */
-
+          int dr1 = r1/2;
+          int dr2 = r2/2;
+          int dr3 = r3/2;
+          int total = dr1 + dr2 + dr3;
+          System.out.print(total);
 
         // closing the scanner object
         scanner.close();
